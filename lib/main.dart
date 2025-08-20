357f15815b4f658c79f1ac32be3deef6f9675813
Helo-Lops16 <heloisalopes560@gmail.com>
2025-08-20 10:50:22 -0300

    criação de model

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todobloc/bloc/task_bloc.dart';
-import 'package:todobloc/ui/screen.dart';
 
+import 'package:todobloc/ui/screen.dart';
 void  main() {
   runApp( 
     MultiBlocProvider(
---
+++ lib/model/model_task.dart
@@ -0,0 +1,18 @@
+
+
+class TaskModel {
+  final String id;
+  final String titulo;
+  final bool feito;
+  final String descricao;
+
+
+  TaskModel({required this.id, 
+  required this.titulo, 
+  required this.feito, 
+  required this.descricao
+    }
+  );
+}
+
+
