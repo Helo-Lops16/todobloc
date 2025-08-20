75016b6f0f335bb78b2520756181502d31319d91
Helo-Lops16 <heloisalopes560@gmail.com>
2025-08-20 10:57:44 -0300

    commit de tristeza

--- lib/bloc/task_bloc.dart
+++ lib/bloc/task_bloc.dart
@@ -6,12 +6,11 @@ import 'package:todobloc/bloc/task_state.dart';
 class TaskBloc extends Bloc<TaskEvent, TaskState> {
   TaskBloc() : super(InicialState()) {
     on<TaskAdded>((event, emit) {
-      // Lógica de adição de tarefa
-      // emit(NewState());
+   
     });
 
     on<TaskRemoved>((event, emit) {
-      // Lógica de remoção de tarefa
+      
       // emit(NewState());
     });
   }
--- lib/model/model_task.dart
+++ lib/model/model_task.dart
@@ -11,6 +11,7 @@ class TaskModel {
   required this.titulo, 
   required this.feito, 
   required this.descricao
+  
     }
   );
 }
